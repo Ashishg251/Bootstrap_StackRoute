d9cfe2debc145f995714156e4a225896e334e272
Ashish Gupta <ashishg251@gmail.com>
2015-12-01 16:15:23 +0530

    Pushing my website with Bootstrap

---
+++ MyWebsite/includes/js/script.js
@@ -0,0 +1,36 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$(function() {
+
+	$('#alertMe').click(function(e) {
+
+		e.preventDefault();
+
+		$('#successAlert').slideDown();
+
+	});
+
+	$('#close').on('click',function(){
+
+		$('#successAlert').slideUp();
+
+	})
+
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+
+	$('a.pop').popover();
+
+	$('[rel="tooltip"]').tooltip();
+
+});
