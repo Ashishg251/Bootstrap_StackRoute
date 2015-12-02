7b86090c8b551abe65340a2017d312c1553113d8
Ashish Gupta <ashishg251@gmail.com>
2015-12-02 11:03:25 +0530

    Done the changes in the portfolio webpage

--- MyWebsite/includes/js/script.js
+++ MyWebsite/includes/js/script.js
@@ -33,4 +33,14 @@ $(function() {
 
 	$('[rel="tooltip"]').tooltip();
 
+	// // ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  // $('.dropdown').on('show.bs.dropdown', function(e){
+  //   $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  // });
+	//
+  // // ADD SLIDEUP ANIMATION TO DROPDOWN //
+  // $('.dropdown').on('hide.bs.dropdown', function(e){
+  //   $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  // });
+
 });
